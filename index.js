957dfcca34861970b96cd0b8e555bc38b103f501
vikas <vikassingh.vs242@gmail.com>
2022-08-18 17:32:12 +0530

    updated count to 20

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const getHello = (str) => {
 
 console.log(getHello("david"));
 
+<<<<<<< HEAD
 for(var i = 0; i <= 10; i++){
 console.log(i)
 }
+=======
+
+for(var i = 0 ; i <= 20 ; i++){
+    console.log(i)
+}
+>>>>>>> 78b611e (make count 1 to 20)
